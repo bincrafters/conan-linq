8e4101675e4f7248dbfe5219070a4c950eca3588
Uilian Ries <uilianries@gmail.com>
2018-10-25 16:38:09 -0300

    #502 Build on linux
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -5,74 +5,26 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ["conan", "libname", "logging"]
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
+class LinqConan(ConanFile):
+    name = "linq"
+    version = "2.0"
+    description = "Linq for list comprehension in C++"
+    homepage = "https://github.com/pfultz2/Linq"
+    topics = ["linq", "list-comprehension", "ranges"]
+    url = "https://github.com/bincrafters/conan-linq"
     author = "Bincrafters <bincrafters@gmail.com>"
-    # Indicates License type of the packaged library
-    license = "MIT"
-
-    # Packages the license for the conanfile.py
-    exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
-    # Custom attributes for Bincrafters recipe conventions
+    license = "BSL-1.0"
+    no_copy_source = True
+    requires = "boost/1.68.0@conan/stable"
     _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-
-    requires = (
-        "OpenSSL/1.0.2p@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
-
-        # Rename to "source_subfolder" is a convention to simplify later steps
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
+        extracted_dir = "Linq-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src=self._source_subfolder)
 
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+    def package_id(self):
+        self.info.header_only()
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,18 @@
 #include <cstdlib>
-#include <iostream>
+#include <cstdio>
+#include <vector>
 
-int main()
-{
-    std::cout << "Bincrafters\n";
+#include <linq.h>
+
+int main() {
+
+    std::vector<int> numbers = { 1, 3, 4, 5 };
+
+    auto q = LINQ(from(number, numbers) where(number % 2));
+
+    for (auto x : q) {
+         printf("%s score: %i\n", x.first.c_str(), x.second);
+    }
+
     return EXIT_SUCCESS;
 }
